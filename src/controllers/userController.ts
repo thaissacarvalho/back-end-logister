c4616beb6406694904ca42b8ce70a6b446e32296
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2024-07-04 16:54:21 -0300

    Changed export method

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,48 +3,50 @@ import User from '../models/User.js';
 import { checkUsername } from '../utils/checkUsername.ts';
 import { cryptPassword } from '../utils/cryptPassword.ts';
 
-export const index = async (req: Request, res: Response) => {
-    try {
-        const users = await User.find();
-        res.json(users);
-    } catch (error) {
-        res.status(500).json({ message: 'Erro ao buscar os usuários' });
-    }
-}
 
-export const register = async (req: Request, res: Response) => {
-    const { name, username, email, password } = req.body
-    try {
-        const usernameExists = await checkUsername(username);
-        const hashedPassword = await cryptPassword(password);
-
-        if (usernameExists) {
-            res.status(400).json({ message: 'Username já existe' });
+const userController = {
+    async index (req: Request, res: Response) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (error) {
+            res.status(500).json({ message: 'Erro ao buscar os usuários' });
         }
-
-        const newUser = new User({
-            name,
-            username,
-            email,
-            password: hashedPassword
-        });
-
-        await newUser.save();
-
-        res.status(201).json({ message: 'O usuário foi criado com sucesso.' })
-
-    } catch (error) {
-        res.status(500).json({ message: 'Erro ao criar o usuário.' });
-        console.log(error);
-    }
-}
-
-export const edit = async (req: Request, res: Response) => {
-    const { userId } = req.params;
-    const { field, value } = req.body;
-
-    try {
-        let updatedUser;
+    },
+    
+    async register (req: Request, res: Response) {
+        const { name, username, email, password } = req.body
+        try {
+            const usernameExists = await checkUsername(username);
+            const hashedPassword = await cryptPassword(password);
+    
+            if (usernameExists) {
+                res.status(400).json({ message: 'Username já existe' });
+            }
+    
+            const newUser = new User({
+                name,
+                username,
+                email,
+                password: hashedPassword
+            });
+    
+            await newUser.save();
+    
+            res.status(201).json({ message: 'O usuário foi criado com sucesso.' })
+    
+        } catch (error) {
+            res.status(500).json({ message: 'Erro ao criar o usuário.' });
+            console.log(error);
+        }
+    },
+    
+    async edit (req: Request, res: Response) {
+        const { userId } = req.params;
+        const { field, value } = req.body;
+    
+        try {
+            let updatedUser;
             switch (field) {
                 case 'name':
                     updatedUser = await User.findByIdAndUpdate(userId, { name: value }, { new: true });
@@ -65,17 +67,20 @@ export const edit = async (req: Request, res: Response) => {
                 default:
                     return res.status(400).json({ error: 'Campo inválido.' });
             }
-
+    
             if (!updatedUser) {
                 return res.status(404).json({ error: 'Usuário não encontrado.' });
             }
-
+    
             return res.json(updatedUser);
-    } catch (error) {
-        if (error instanceof Error) {
-            res.status(500).json({ message: `${error.message}` });
-        } else {
-            res.status(500).json({ message: 'Server error' });
+        } catch (error) {
+            if (error instanceof Error) {
+                res.status(500).json({ message: `${error.message}` });
+            } else {
+                res.status(500).json({ message: 'Server error' });
+            }
         }
     }
 }
+
+export default userController;
