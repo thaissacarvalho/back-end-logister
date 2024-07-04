797325389a610b45be5704d9868d06850b58c757
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2024-07-04 16:11:49 -0300

    Improving user verification logic and password cryptography.

--- src/utils/checkUsername.ts
+++ src/utils/checkUsername.ts
@@ -1,11 +1,10 @@
 import User from "../models/User.js";
 
-export const checkUsername = async (username: string): Promise<boolean> => {
+export const checkUsername = async (username: string) => {
     try {
         const existingUser = await User.findOne({ username });
         return !!existingUser;
     } catch (error) {
         console.error({ message: 'Erro ao verificar existência de usuário.', error });
-        return true;
     }
 }
--- src/utils/cryptPassword.ts
+++ src/utils/cryptPassword.ts
@@ -1,10 +1,10 @@
 import bcrypt from 'bcryptjs';
 
-export const cryptPassword = async (password: string): Promise<string> => {
+export const cryptPassword = async (password: string) => {
     try {
         const hashedPassword:string = await bcrypt.hash(password, 10);
         return hashedPassword;
     } catch (error) {
-        throw new Error('Erro ao aceitar a senha.');
+        throw new Error('Erro ao criptografar a senha.');
     }
 }
