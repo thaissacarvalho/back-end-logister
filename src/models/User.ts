9ab0754d39b8cacc3c0afba679c3094c96ea0623
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2024-07-31 18:52:12 -0300

    Modified file with comparePassword and bcrypt

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,10 +1,12 @@
 import mongoose, { Schema, Document } from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 export interface User extends Document {
     name: string,
     username: string,
     email: string,
-    password: string
+    password: string,
+    comparePassword: (password: string) => Promise<boolean>;
 }
 
 const UserSchema: Schema<User> = new Schema({
@@ -27,5 +29,16 @@ const UserSchema: Schema<User> = new Schema({
         required: true
     }
 });
+
+UserSchema.pre<User>('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
+
+UserSchema.methods.comparePassword = function (password: string) {
+    return bcrypt.compare(password, this.password);
+};
 
 export default mongoose.model<User>('User', UserSchema);
